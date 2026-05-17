fe0847b563ad71dedf9f81789de7d9e433a33ccc
proy05 <79052853+proy05@users.noreply.github.com>
2026-05-18 01:33:15 +0530

    chore(refactor): Switch to using Spring HTTP Interface/service client instead of OpenFiegn to call Inventory service

--- order-service/src/main/java/com/roypr/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/roypr/microservices/order/OrderServiceApplication.java
@@ -2,10 +2,8 @@ package com.roypr.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients //enable OpenFeign REST clients(declarative client instead of imperative) in the application
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
